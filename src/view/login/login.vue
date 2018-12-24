d0e661aa40199f2ff9cb7e8da7692cb7333b32d6
chenxin <517242860@qq.com>
2018-12-24 09:11:06 +0800

    更新

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -13,7 +13,12 @@
           :max=20
           @on-change="keyDown"
           placeholder="请输入用户名..."
-          v-model="userInfo.username"></x-input>
+          v-model="userInfo.username">
+          <span class="icon"
+            slot="label">
+            <i class="iconfont icon-user"></i>
+          </span>
+        </x-input>
         <x-input class="input"
           ref="password"
           title=""
@@ -21,26 +26,44 @@
           :min=6
           :max=16
           @on-change="keyDown"
-          type="password"
+          :type="pwdType"
           placeholder="请输入密码..."
-          v-model="userInfo.password"></x-input>
+          v-model="userInfo.password">
+          <span class="icon"
+            slot="label">
+            <i class="iconfont icon-password"></i>
+          </span>
+          <span class="showpw"
+            slot="right"
+            @click="changeType">
+            <i class="iconfont icon-eye"
+              :style="{ color: pwdColor }"></i>
+          </span>
+        </x-input>
         <x-button class="sign-in"
           :disabled="disabled"
           type="primary"
-          @click.native="signIn()">登录</x-button>
+          @click.native="signIn()">
+          <span class="btn-loading"
+            v-if="loading">
+            <img src="../../assets/tail-spin.svg">加载中
+          </span>
+          <span v-else>登录</span>
+        </x-button>
       </group>
     </div>
   </div>
 </template>
 
 <script>
-import { Group, XInput, XButton } from 'vux'
+import { Group, XInput, XButton, InlineLoading } from 'vux'
 export default {
   name: 'login',
   components: {
     Group,
     XInput,
-    XButton
+    XButton,
+    InlineLoading
   },
   data () {
     return {
@@ -48,25 +71,20 @@ export default {
         username: 'admin',
         password: '123456'
       },
-      disabled: true
+      disabled: true,
+      pwdType: 'password',
+      pwdColor: '#B2B2B2',
+      loading: false
     }
   },
   methods: {
     signIn () {
       if (this.userInfo.namename !== '' && this.userInfo.password !== '') {
+        this.loading = true
         this.$store.dispatch('Login', this.userInfo).then(() => {
           this.$router.push({ path: '/' })
+          this.loading = false
         })
-        // let data = {
-        //   username: this.username,
-        //   password: md5(this.password)
-        // }
-        // getLogin(data).then((res) => {
-        //   res = res.data.data
-        //   if (res.err_code === ERR_OK) {
-        //     this.$router.push('./article')
-        //   }
-        // })
       }
     },
     keyDown () {
@@ -75,12 +93,22 @@ export default {
       } else {
         this.disabled = true
       }
+    },
+    changeType () {
+      if (this.pwdType === 'password') {
+        this.pwdType = 'text'
+        this.pwdColor = '#41b883'
+      } else {
+        this.pwdType = 'password'
+        this.pwdColor = '#B2B2B2'
+      }
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+@import "src/assets/styles/color.scss";
 .form /deep/ {
   .weui-cells:before,
   .weui-cells:after,
@@ -94,10 +122,13 @@ export default {
     border-width: 0;
   }
   .weui-btn_primary {
-    background: #41b883;
+    background: $default_color;
   }
   .weui-btn_disabled.weui-btn_primary {
-    background: #80d2ae;
+    background: $default_light_color;
+  }
+  .weui-btn_primary:not(.weui-btn_disabled):active {
+    background: $default_dark_color;
   }
 }
 .logo {
@@ -118,6 +149,42 @@ export default {
   }
   .sign-in {
     margin-top: 20px;
+  }
+}
+.showpw {
+  display: inline-flex;
+  vertical-align: middle;
+  justify-content: center;
+  align-items: center;
+  width: 24px;
+  height: 24px;
+  i {
+    display: block;
+    font-size: 22px;
+  }
+}
+.icon {
+  display: inline-flex;
+  vertical-align: middle;
+  justify-content: center;
+  align-items: center;
+  width: 24px;
+  height: 24px;
+  margin-right: 15px;
+  i {
+    display: block;
+    font-size: 24px;
+  }
+}
+.btn-loading {
+  display: inline-block;
+  height: 20px;
+  line-height: 20px;
+  font-size: 14px;
+  img {
+    height: 20px;
+    margin-right: 8px;
+    vertical-align: middle;
   }
 }
 </style>
